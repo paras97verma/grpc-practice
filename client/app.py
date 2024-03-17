26492a393c5d7b3ebbd670dce4f2218639c890a5
Paras Verma <peivee@Parass-MacBook-Air.local>
2024-03-17 10:21:38 +0530

    Format

--- client/app.py
+++ client/app.py
@@ -4,8 +4,9 @@ import grpc
 import users_pb2
 import users_pb2_grpc
 
+
 def run():
-    with grpc.insecure_channel('localhost:50051') as channel:
+    with grpc.insecure_channel("localhost:50051") as channel:
         stub = users_pb2_grpc.UsersStub(channel)
         response = stub.GetUsersUser(users_pb2.GetUsersRequest())
     print(response.users)
