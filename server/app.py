d4acf806d53f8c11e0cf289819016e8fd291d3f6
Paras Verma <peivee@Parass-MacBook-Air.local>
2024-03-17 10:11:52 +0530

    Changed the type

--- server/app.py
+++ server/app.py
@@ -11,7 +11,7 @@ class Users(users_pb2_grpc.UsersServicer):
         return users_pb2.GetUsersResponse(
             users=[
                 users_pb2.User(
-                    id=1,
+                    id= '1',
                     name="Paras Verma",
                     email="paras@verma.com",
                     password="paras@123",
