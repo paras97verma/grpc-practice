235969858a57911a3c9a5ae80c5b88b0cc8b7d2f
Paras Verma <peivee@Parass-MacBook-Air.local>
2024-03-17 10:31:43 +0530

    modified

--- client/app.py
+++ client/app.py
@@ -8,5 +8,9 @@ import users_pb2_grpc
 def run():
     with grpc.insecure_channel("localhost:50051") as channel:
         stub = users_pb2_grpc.UsersStub(channel)
-        response = stub.GetUsersUser(users_pb2.GetUsersRequest())
+        response = stub.GetUsers(users_pb2.GetUsersRequest())
     print(response.users)
+
+
+if __name__ == "__main__":
+    run()
